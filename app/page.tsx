7a7bc6218d0c89ac049c7a9e11868c9f68753e30
Sakpal Amit <amitsakpal744@gmail.com>
2024-11-03 12:15:33 +0530

    implemented our demo logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
 import { ThemeProvider } from "@/components/theme-provider";
-
+import Image from "next/image";
 
 export default function Home() {
   return (
      <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
       <div className="min-h-screen p-8 pb-20 gap-16 sm:p-12 font-[family-name:var(--font-geist-sans)] bg-background text-foreground ">
-        
+       <header>
+        <Image
+        className="mx-auto text-white mb-4"
+        src="/svg/lock-square-rounded.svg"
+        alt="CLOUD3 logo"
+        width={70}
+        height={30}
+        priority
+        />
+       </header>
       </div>
      </ThemeProvider>
   );
