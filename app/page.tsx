35fc1175b1d209d9e477555270b6149e19581817
Sakpal Amit <amitsakpal744@gmail.com>
2024-11-03 14:14:13 +0530

    fixed the error imports created starting hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import { ThemeProvider } from "@/components/theme-provider";
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 export default function Home() {
