4e3203a741073d373ef6843be5645dc2f689b86b
Sakpal Amit <amitsakpal744@gmail.com>
2024-11-03 11:43:55 +0530

    wrapped the ThemeProvider

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { ThemeProvider } from "@/components/theme-provider";
 
 
 export default function Home() {
   return (
-    <h2>Hello Cloud3</h2>
+     <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+      <div className="min-h-screen p-8 pb-20 gap-16 sm:p-12 font-[family-name:var(--font-geist-sans)] bg-background text-foreground ">
+        
+      </div>
+     </ThemeProvider>
   );
 }
