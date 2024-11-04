0e173b1c736bbc0bfa8da952c13e32447ec4a4e4
Sakpal Amit <amitsakpal744@gmail.com>
2024-11-04 12:55:40 +0530

    created schemas for webpages and deployment as well

--- utils/db/schema.ts
+++ utils/db/schema.ts
@@ -8,3 +8,22 @@ export const Users = pgTable("users", {
     updatedAt: timestamp("updated_at").defaultNow(),
     lastLogin: timestamp("last_login"),
   });
+
+  export const Webpages = pgTable("webpages", {
+    id: serial("id").primaryKey(),
+    userId: integer("user_id").references(() => Users.id),
+    name: text("name"),
+    domain: varchar("domain", { length: 255 }).notNull(),
+    cid: varchar("cid", { length: 255 }).notNull(),
+  });
+  
+  export const Deployments = pgTable("deployments", {
+    id: serial("id").primaryKey(),
+    userId: integer("user_id").references(() => Users.id),
+    webpageId: integer("webpage_id").references(() => Webpages.id),
+    transactionHash: varchar("transaction_hash", { length: 66 }).notNull(),
+    deployedAt: timestamp("deployed_at").defaultNow(),
+    deploymentUrl: varchar("deployment_url", { length: 255 }).notNull(),
+    filecoinInfo: text("filecoin_info"), // New field to store Filecoin information
+  });
+  
