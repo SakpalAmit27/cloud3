43876d951272e59efb6ffdaa211c7918a96ca489
Sakpal Amit <amitsakpal744@gmail.com>
2024-11-04 08:22:03 +0530

    added some other options in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,22 @@
 import Link from "next/link"
 import Image from "next/image"
+import { Button } from "./ui/button"
+import { usePrivy } from "@privy-io/react-auth"
+import { LogIn, LogOut } from "lucide-react";
 
 export default function Navbar(){
+    const {login , logout , authenticated , user} = usePrivy();
+
+    const handleAuth = () => {
+      if(authenticated){
+        logout();
+      }
+      else{
+        login();
+      }
+    }
+
+    
     return(
     <>
     <nav className="flex justify-between items-center mb-1 py-2 pt-4 px-4 md:px-8 lg:px-16">
@@ -17,6 +32,31 @@ export default function Navbar(){
         </Link>
         <div className="hidden md:flex items-center space-x-4">
 
+        <Link
+            href="/dashboard"
+            className="text-foreground hover:text-primary"
+          >
+            Dashboard
+          </Link>
+          <Link href="/search" className="text-foreground hover:text-primary">
+            Search
+          </Link>
+          <Link href="/docs" className="text-foreground hover:text-primary">
+            Docs
+          </Link>
+          <Button onClick={handleAuth} variant="outline">
+            {authenticated ? (
+              <>
+                <LogOut className="mr-2 h-4 w-4" />
+                Logout
+              </>
+            ) : (
+              <>
+                <LogIn className="mr-2 h-4 w-4" />
+                Login
+              </>
+            )}
+          </Button>
         </div>
     </nav>
     </>
