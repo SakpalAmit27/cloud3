35b662deac4268cd90d6a66ccb677802e1b0a7b0
Sakpal Amit <amitsakpal744@gmail.com>
2024-11-07 19:17:43 +0530

    removed the use server and exposed the db

--- utils/db/dbConfig.jsx
+++ utils/db/dbConfig.jsx
@@ -1,9 +1,11 @@
-"use server"
+
 
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 
-const sql = neon(process.env.DATABASE_URL);
+const sql = neon(
+    "postgresql://neondb_owner:rduePhDY93gf@ep-muddy-flower-a5w6itas.us-east-2.aws.neon.tech/neondb?sslmode=require"
+);
 
 export const db = drizzle(sql, { schema });
