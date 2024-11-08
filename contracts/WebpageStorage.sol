5d0667c8cac80be3dfc0a37edaeee33ef3fee3d9
Sakpal Amit <amitsakpal744@gmail.com>
2024-11-08 14:12:11 +0530

    a function to check if cid and domain should exist if not returning the message

--- contracts/WebpageStorage.sol
+++ contracts/WebpageStorage.sol
@@ -16,9 +16,11 @@ contract WebpageStorage{
 
     event WebpageStored(string domain,string cid,address owner,uint256 timestamp);
 
+    function storeWebpages(string memory domain , string memory cid)public{
 
+        // checkinf if domain and cid is not empty 
+        require(bytes(domain).length > 0,"Domain cannot be empty"); 
+        require(bytes(cid).length > 0,"CID cannot be empty"); 
 
-  
-
-
+    }
 }
