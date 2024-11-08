5dc5e039f76b39e9e7b0580c1d9e1d385d674349
Sakpal Amit <amitsakpal744@gmail.com>
2024-11-08 14:49:55 +0530

    added function getwebpage to get the struct

--- contracts/WebpageStorage.sol
+++ contracts/WebpageStorage.sol
@@ -22,5 +22,19 @@ contract WebpageStorage{
         require(bytes(domain).length > 0,"Domain cannot be empty"); 
         require(bytes(cid).length > 0,"CID cannot be empty"); 
 
+
+        webpages[domain] = WebPage(cid,msg.sender,block.timestamp);
+        userWebpages[msg.sender].push(domain); 
+
+        emit WebpageStored(domain,cid,msg.sender,block.timestamp);
+
+
+         
+    } 
+
+    function getWebpage(string memory domain)public view returns(string memory,address,uint256){
+        WebPage memory webpage = webpages[domain],
+        require(bytes(webpage.cid).length > 0,"Webpage not found");
+        return (webpage.cid,webpage.owner,webpage.timestamp);
     }
 }
