7dd412acb5a5980011f0b0ff0eb93ebec00da274
Sakpal Amit <amitsakpal744@gmail.com>
2024-11-08 08:26:53 +0530

    creating a contract , making structure in progress

---
+++ contracts/WebpageStorage.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.8.24;
+
+
+// creating contract // 
+contract WebpageStorage{
+
+    struct WebPage{
+        string cid; 
+        address owner; 
+        uint256 timestamp; 
+
+    }
+    
+}
